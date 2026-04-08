08e6111d7684714a8f31bdc27bcc2593823b899b
Max Clayton Clowes <contact@mcclowes.com>
2026-04-08 23:27:59 +0100

    Add barred cask formula

---
+++ Casks/barred.rb
@@ -0,0 +1,17 @@
+cask "barred" do
+  version "0.1.0"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/mcclowes/barred/releases/download/v#{version}/Barred.zip"
+  name "Barred"
+  desc "macOS menu bar manager"
+  homepage "https://github.com/mcclowes/barred"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Barred.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.barred.app.plist",
+  ]
+end
