d9e800d49ab0cfa39e784b93d93cdcd25c6c4abb
Max Clayton Clowes <max@mcclowes.com>
2026-07-08 14:27:21 +0000

    Update barred to v1.1.3

--- Casks/barred.rb
+++ Casks/barred.rb
@@ -1,6 +1,6 @@
 cask "barred" do
-  version "1.1.2"
-  sha256 "cb830899812d653530c0f1a6869bdebbc74b53ebf59f867de3723380366df260"
+  version "1.1.3"
+  sha256 "15e2663b11bb6e04e19520cda97140b278f5ef7366fda56cd931b7615a82b693"
   url "https://github.com/mcclowes/barred/releases/download/v#{version}/Barred.zip"
   name "Barred"
   desc "Native macOS menu bar manager"
